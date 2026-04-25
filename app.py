04f7343c94a57cbdb27cab7fae57629001504dd0
maguijim@posgrado.upv.es <maguijim@posgrado.upv.es>
2026-04-25 14:41:39 -0600

    Changes to update and fix bug about popularity

--- app.py
+++ app.py
@@ -48,11 +48,24 @@ with tab2:
     st.pyplot(fig_corr)
 
 with tab3:
-    st.subheader("Hits vs. Atributos")
-    fig_bubble = px.scatter(display_df, x="valence", y="energy", size="popularity", 
-                            color="album", hover_name="name")
-    st.plotly_chart(fig_bubble, use_container_width=True)
+   st.subheader("Hits vs. Atributos")
+    
+    # --- SOLUCIÓN: Creamos una copia para no romper el original y sumamos 1 ---
+    # Esto asegura que el tamaño mínimo sea 1 y no 0
+    temp_df = display_df.copy()
+    temp_df['popularity_size'] = temp_df['popularity'].fillna(0) + 1 
 
+    fig_bubble = px.scatter(
+        temp_df, 
+        x="valence", 
+        y="energy", 
+        size="popularity_size",  # Usamos la nueva columna con valores > 0
+        color="album", 
+        hover_name="name",
+        # Añadimos esto para que las burbujas no se vean minúsculas
+        size_max=15 
+    )
+    st.plotly_chart(fig_bubble, use_container_width=True)
 # ---------------------------------------------------------
 # 5. SECCIÓN DE LETRAS (Al final de todo)
 # ---------------------------------------------------------
